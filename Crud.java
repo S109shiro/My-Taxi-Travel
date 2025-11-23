9e2fb739c4ed3f44a4ddc06b31ec35cb34eb3920
S109shiro <sebgonrod@gmail.com>
2025-11-22 23:26:00 -0500

    Se agrega la funcionalidad de leer los datos de un usuario

--- Crud.java
+++ Crud.java
@@ -25,7 +25,36 @@ public class Crud {
         }
     }
 
-    public void modificarUsuario(){
+    public void leerDatosUsuario(){
+        String salida = "ID: %d, Nombre: %s, Primer apellido: %s, Segundo apellido: %s, Edad: %d, Numero identificacion: %d, Email: %s, Sexo: %s, Documento de identidad: %s, Numero de telefono: %s, Fecha de nacimiento: %s, Calificacion media: %f, Historial de viajes: %s";
+        String query = "select * from usuario";
+        try{
+            Connection conexion = ConexionDB.conectar();
+            PreparedStatement preparacion = conexion.prepareStatement(query);
+            ResultSet resultado = preparacion.executeQuery();
 
+            while(resultado.next()){
+                int id = resultado.getInt("id_usuario");
+                String nombre = resultado.getString("nombre");
+                String primer_apellido = resultado.getString("primer_apellido");
+                String segundo_apellido = resultado.getString("segundo_apellido");
+                byte edad = resultado.getByte("edad");
+                int numero_identificacion = resultado.getInt("numero_identificacion");
+                String email = resultado.getString("email");
+                String sexo = resultado.getString("sexo");
+                String documento_identidad = resultado.getString("documento_identidad");
+                String numero_telefono = resultado.getString("numero_telefono");
+                String fecha_nacimiento = resultado.getString("fecha_nacimiento");
+                float calificacion_media = resultado.getFloat("calificacion_media");
+                String historial_viajes = resultado.getString("historial_viajes");
+
+                System.out.println(String.format(salida, id, nombre, primer_apellido, segundo_apellido, edad, numero_identificacion, email, sexo, documento_identidad, numero_telefono, fecha_nacimiento, calificacion_media , historial_viajes));
+            }
+
+
+        }catch (SQLException ex){
+            System.err.println("Error al mostrar los datos");
+            ex.printStackTrace();
+        }
     }
 }
--- Pruebas.java
+++ Pruebas.java
@@ -3,11 +3,13 @@ import java.sql.Date;
 public class Pruebas {
     public static void main(String[] args){
         // Creacion de un objeto de la clase ConexionDB
-        ConexionDB conexion = new ConexionDB();
-        conexion.conectar();
+
+        //ConexionDB conexion = new ConexionDB();
+        //conexion.conectar();
 
         Crud crear = new Crud();
-        crear.crearUsuario("Reo", "Gonzalez", "Rodriguez", (byte) 21, 20879223,"shola@gmail.com", "Femboy", "https//documento.pdf", "3003367605", Date.valueOf("2004-10-09"), (float)4.8, "[\"Bogota\", \"Medellin\"]");
+        //crear.crearUsuario("Reo", "Gonzalez", "Rodriguez", (byte) 21, 20879223,"shola@gmail.com", "Femboy", "https//documento.pdf", "3003367605", Date.valueOf("2004-10-09"), (float)4.8, "[\"Bogota\", \"Medellin\"]");
+        crear.leerDatosUsuario();
 
     }
 }
