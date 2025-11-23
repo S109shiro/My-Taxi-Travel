f692deb3ea3268576ba50f6054b8ac021db0a341
S109shiro <sebgonrod@gmail.com>
2025-11-22 20:12:38 -0500

    Conexion a la BD exitosa

--- ConexionDB.java
+++ ConexionDB.java
@@ -1,4 +1,22 @@
+import java.sql.*;
 public class ConexionDB {
+    // Se agrega el static para que sean variables de la clase solamente
+    private static String url = "jdbc:mysql://localhost:3306/my_taxi_travel";
+    private static String user = "root";
+    private static String pass = "2004248";
 
+    // Creamos un metodo para conectarnos a la db
+    public static Connection conectar(){
+        Connection conexion = null;
+        try{
+            conexion = DriverManager.getConnection(url, user, pass);
+            System.out.println("La conexion ha sido exitosa");
+        }catch (SQLException e){
+            // Nos ayuda a ver los registros donde le programa tuvo un error.
+            e.printStackTrace();
+            System.err.println("Hubo un error en la conexion");
+        }
+        return conexion;
+    }
 
 }
--- Pruebas.java
+++ Pruebas.java
@@ -1,2 +1,7 @@
 public class Pruebas {
+    public static void main(String[] args){
+        // Creacion de un objeto de la clase ConexionDB
+        ConexionDB conexion = new ConexionDB();
+        conexion.conectar();
+    }
 }
